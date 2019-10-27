5e8e16910e3740187d8901495eecfa8506d89bfd
Everspace <7560635+Everspace@users.noreply.github.com>
2019-10-27 03:57:11 -0700

    Add missing module declaration

--- Modules.d.ts
+++ Modules.d.ts
@@ -2,3 +2,9 @@ declare module "*.gql" {
   const value: string
   export default value
 }
+
+// CONTRIB: add-page-dependency should have this declared.
+declare module "gatsby/dist/redux/actions/add-page-dependency" {
+  const value: any
+  export default value
+}
