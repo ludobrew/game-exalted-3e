53e2aa171187c48e61f127d1ffa13cd46ba27225
Everspace <7560635+Everspace@users.noreply.github.com>
2019-11-14 00:15:09 -0800

    Create BreadCrumbBar

--- src/components/Common.tsx
+++ src/components/Common.tsx
@@ -1,7 +1,32 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
 import { GatsbyLinkProps, Link } from "gatsby"
+import { gameId, gameShortName } from "../../data.json"
+import { intersperse } from "../lib"
+import { pathify } from "@ludobrew/core/gatsbyNodeTools"
 
 export const StyledLink = (props: GatsbyLinkProps<unknown>) => (
   <Styled.a as={Link} {...(props as any)} />
 )
+
+type BreadCrumbBarProps = {
+  to?: string[]
+}
+
+export const BreadCrumbBar = ({ to = [] }: BreadCrumbBarProps) => {
+  const links = to.map((item, index, array) => (
+    <StyledLink key={item} to={pathify(gameId, ...array.slice(0, index))}>
+      {item}
+    </StyledLink>
+  ))
+  return (
+    <Styled.div>
+      <StyledLink key={gameId} to={pathify(gameId)}>
+        {gameShortName}
+      </StyledLink>
+      {links.length > 0 ? " / " : null}
+      {[...intersperse(links, " / ")]}
+      {" / "}
+    </Styled.div>
+  )
+}
