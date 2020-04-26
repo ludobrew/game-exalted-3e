de14c252a7ff28fbc6d2b672514d150f145e8f6c
Everspace <7560635+Everspace@users.noreply.github.com>
2020-04-25 20:11:40 -0700

    Extract trait list to other component

---
+++ src/components/SplatPage/TraitList.tsx
@@ -0,0 +1,77 @@
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui"
+import { graphql, useStaticQuery } from "gatsby"
+import { useMemo } from "react"
+import { StyledLink } from "../Common"
+import { pathify } from "gatsby-theme-ludobrew-core/gatsbyNodeTools"
+
+const defaultComparator = (a: string, b: string) => {
+  if (a === "Universal") return -1
+  if (b === "Universal") return 1
+  return a.localeCompare(b)
+}
+
+type TraitlistData = {
+  splat: string
+  traitList: {
+    traits: { name: string; count: number }[]
+  }
+}
+
+const TraitList = ({ splat, traitList }: TraitlistData) => {
+  // Todo, make sort order configurable
+  // const data = useStaticQuery(graphql`
+  //   query TraitListSortOrderData {
+  //     site {
+  //       siteMetadata {
+  //         traitPriority
+  //       }
+  //     }
+  //   }
+  // `)
+  const data = {} as any
+
+  const traitOrder = useMemo(() => {
+    const traitPriority: string[] =
+      data?.site?.siteMetadata?.traitPriority ?? []
+    if (traitPriority.length === 0) {
+      return defaultComparator
+    }
+
+    return (a: string, b: string) => {
+      const indexA = traitPriority.indexOf(a)
+      const indexB = traitPriority.indexOf(b)
+      if (indexA >= 0 && indexB >= 0) return indexA - indexB // I think
+      if (indexA >= 0) return -1
+      if (indexB >= 0) return 1
+      return a.localeCompare(b)
+    }
+  }, [data])
+
+  return (
+    <Styled.ul>
+      {traitList.traits
+        .sort((a, b) => traitOrder(a.name, b.name))
+        .map(({ name, count }) => (
+          <Styled.li key={name}>
+            <StyledLink to={pathify(splat, name)}>{name}</StyledLink>
+            {" - "}
+            {count} Charm{count == 1 ? "" : "s"}
+          </Styled.li>
+        ))}
+    </Styled.ul>
+  )
+}
+
+export default TraitList
+
+export const TraitListRequiredDataFragment = graphql`
+  fragment TraitListRequiredDataFragment on Query {
+    traitList: allExaltedCharm(filter: { charmSource: { eq: $splat } }) {
+      traits: group(field: trait) {
+        name: fieldValue
+        count: totalCount
+      }
+    }
+  }
+`
--- src/components/SplatPageLayout.tsx
+++ src/components/SplatPageLayout.tsx
@@ -6,9 +6,13 @@ import { pathify } from "gatsby-theme-ludobrew-core/gatsbyNodeTools"
 import { OptionalMdxRenderer } from "gatsby-theme-ludobrew-core/src/components/OptionalMdxRenderer"
 import Layout from "./Layout"
 import { StyledLink, BreadCrumbBar } from "./Common"
+import TraitList from "./SplatPage/TraitList"
 
-const traitOrder = (a: string, b: string) =>
-  a === "Universal" ? -1 : a.localeCompare(b)
+const traitOrder = (a: string, b: string) => {
+  if (a === "Universal") return -1
+  if (b === "Universal") return 1
+  return a.localeCompare(b)
+}
 
 const SplatPageLayout: React.FC<any> = ({ data, pageContext }) => {
   const { traitList, preface } = data
@@ -20,17 +24,7 @@ const SplatPageLayout: React.FC<any> = ({ data, pageContext }) => {
       <Styled.h1>{splat}</Styled.h1>
       <OptionalMdxRenderer mdxNode={preface} />
       <Styled.h2>Charms</Styled.h2>
-      <Styled.ul>
-        {traitList.traits
-          .sort((a, b) => traitOrder(a.name, b.name))
-          .map(({ name, count }) => (
-            <Styled.li key={name}>
-              <StyledLink to={pathify(splat, name)}>{name}</StyledLink>
-              {" - "}
-              {count} Charm{count == 1 ? "" : "s"}
-            </Styled.li>
-          ))}
-      </Styled.ul>
+      <TraitList splat={splat} traitList={traitList} />
     </Layout>
   )
 }
